1b5dccd1f91c8082c79e614f85a94222f3593e45
Anamika <anamikacrew8@gmail.com>
2023-07-02 16:20:53 +0545

    recipe-page-modiffied

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,20 @@
 <template>
 <div class="bg">
-    <div class=" container m-auto py-2 ">
-        <h1 class=" text-center text-white text-3xl font-bold py-2 ">Anamikacrew</h1>
-        <p class="text-white text-center text-xl">Welcome to our recipe website, your one-stop destination for culinary inspiration! Whether <br>
-             you're an experienced chef or just starting your culinary journey, we have a wide array of <br> mouthwatering recipes to tantalize your taste buds. From delectable appetizers to hearty main <br>
-              courses and indulgent desserts, our collection covers a diverse range of cuisines and dietary <br>
-               preferences. Explore our carefully curated selection of recipes, complete with detailed <br>
-                instructions, ingredient lists, and helpful tips to ensure your culinary success. We believe that <br> cooking should be a joyous and creative experience, so we're here to empower you with the knowledge <br>
-                 and inspiration to create delicious meals that will impress your family and friends. So, roll up <br>
-                  your sleeves, grab your apron, and let's embark on a flavorful adventure together!
+    <div class="my-component  py-2  ">
+        <h1 class=" text-start text-white text-3xl font-bold py-2 px-40 ">Anamikacrew</h1>
+        <p class="text-white text-start text-xl px-2">
+            Welcome to our recipe website, your one-stop destination for culinary <br>
+            inspiration! Whether you're an experienced chef or just starting your <br>
+            culinary journey, we have a wide array of  mouthwatering recipes to <br>
+            tantalize your taste buds. From delectable appetizers to hearty main <br> 
+            courses and indulgent desserts, our collection covers a diverse range of <br>
+            cuisines and dietary  preferences. Explore our carefully curated selection <br>
+            of recipes, complete with detailed instructions, ingredient lists, and <br>
+            helpful tips to ensure your culinary success. We believe that  cooking <br>
+            should be a joyous and creative experience, so we're here to empower <br>
+            you with the knowledge and inspiration to create delicious meals that <br>
+            will impress your family and friends. So, roll up  your sleeves, grab <br>
+             your apron, and let's embark on a flavorful adventure together!
 
 
 
@@ -26,4 +32,18 @@
 
 <style  scoped>.bg {
     background-color: #302414;
-}</style>
+}
+.my-component {
+  background-image: url('~/assets/images/background.jpg');
+  background-size: cover;
+  background-repeat: none;
+  height: 100vh;
+  width: 100%;
+  overflow: hidden;
+  /* Additional CSS properties for styling the background */
+}
+.my-component img{
+    object-fit: cover;
+  /* Additional CSS properties for styling the background */
+}
+</style>
